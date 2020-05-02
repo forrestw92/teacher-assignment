627a44f6af9c5dd9f50cdb840b49e39dc1200166
Forrest Walker <me@forrestwalker.net>
2020-05-02 18:24:43 -0500

    Added createStudent method

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,20 @@
+import {Args, Mutation, Resolver} from "@nestjs/graphql";
+import {StudentType} from "./student.type";
+import {StudentService} from "./student.service";
+import {Student} from "./student.entity";
+import {CreateStudentInput} from "./create-student.input";
+
+@Resolver(of => StudentType)
+export class StudentResolver {
+    constructor(
+        private studentService: StudentService
+    ) {}
+
+    @Mutation(returns => StudentType)
+    createStudent(
+        @Args('createStudentInput') createStudentInput: CreateStudentInput
+    ) {
+        return this.studentService.createStudent(createStudentInput)
+    }
+
+}
